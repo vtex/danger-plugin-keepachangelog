8e6bac46c0d2e1ddc9c36f3e8cd631c5615fb43e
Breno Calazans <brenocalazans@gmail.com>
2020-03-21 22:51:35 -0300

    Fix list markdown

--- src/message.ts
+++ src/message.ts
@@ -10,12 +10,12 @@ ${versionChange ? `\n+## [1.0.1] - ${(new Date).toISOString().slice(0, 10)}` : '
 \`\`\`
 
 Possible types of changes:
-- \`Added\` for new features.
-- \`Changed\` for changes in existing functionality.
-- \`Deprecated\` for soon-to-be removed features.
-- \`Removed\` for now removed features.
-- \`Fixed\` for any bug fixes.
-- \`Security\` in case of vulnerabilities.
+-  \`Added\` for new features.
+-  \`Changed\` for changes in existing functionality.
+-  \`Deprecated\` for soon-to-be removed features.
+-  \`Removed\` for now removed features.
+-  \`Fixed\` for any bug fixes.
+-  \`Security\` in case of vulnerabilities.
 `
 
 export const missingVersion = `We follow [keepachangelog](https://keepachangelog.com/) standards.
@@ -49,10 +49,10 @@ ${versionChange ? `\n+## [1.0.1] - ${(new Date).toISOString().slice(0, 10)}` : '
 \`\`\`
 
 You can use one of the following sections:
-- \`Added\` for new features.
-- \`Changed\` for changes in existing functionality.
-- \`Deprecated\` for soon-to-be removed features.
-- \`Removed\` for now removed features.
-- \`Fixed\` for any bug fixes.
-- \`Security\` in case of vulnerabilities.
+-  \`Added\` for new features.
+-  \`Changed\` for changes in existing functionality.
+-  \`Deprecated\` for soon-to-be removed features.
+-  \`Removed\` for now removed features.
+-  \`Fixed\` for any bug fixes.
+-  \`Security\` in case of vulnerabilities.
 `
